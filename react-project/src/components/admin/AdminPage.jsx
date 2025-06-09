1ea328c125d0e79b72da772dd0779d83db37df6f
Rocomi <kauli1014@naver.com>
2025-06-09 09:29:19 +0900

    관리자페이지 사이드바 추가(미완)

--- react-project/src/components/admin/AdminPage.jsx
+++ react-project/src/components/admin/AdminPage.jsx
@@ -1,13 +1,15 @@
 import '../../css/admin/AdminPage.css';
+import AdminSideBar from './AdminSideBar';
 
 const AdminPage = () => {
    
     return (
-    <>
+    <div className='adminPage'>
+    <AdminSideBar/>
         <h1>
             관리자 페이지 입니다.
         </h1>
-    </>
+    </div>
     );
 };
 
---
+++ react-project/src/components/admin/AdminSideBar.jsx
@@ -0,0 +1,91 @@
+import { NavLink } from 'react-router-dom';
+import {  useState } from 'react';
+import "../../css/admin/AdminSideBar.css";
+
+const AdminSideBar = () => {
+
+  // 드롭 다운 메뉴
+  const [dropDownVisible, setDropDown] = useState(false);
+
+  return (
+    <div className="adminSideBar">
+      <div className="sideBarWrapper">
+        <div className="sideBarMenu">
+          <h2 className="sideBarTitle">JobFolio</h2>
+          <hr className="menuDivider" />
+          <ul className="sideBarMenuList">
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                사이트 바로가기
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                회원 관리
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                관리자 계정 관리
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                이용권 현황
+              </NavLink>
+            </li>
+            <li className="list-item" onClick={(e) => setDropDown(!dropDownVisible)}>
+              <span className="dropDownMenuTitle">
+                커뮤니티 관리
+                {
+                  dropDownVisible ? "∧" : "∨"
+                }
+              </span>
+            </li>
+            <div className={`sideBarDropDownMenuList ${dropDownVisible ? 'slide-fade-in-dropdown' : 'slide-fade-out-dropdown'}`}>
+              <ul>
+                <li className="list-item">
+                  <NavLink to="/" activeClassName="list-active">
+                    공지사항
+                  </NavLink>
+                </li>
+                <li className="list-item">
+                  <NavLink to="/" activeClassName="list-active">
+                    이력서
+                  </NavLink>
+                </li>
+                <li className="list-item">
+                  <NavLink to="/" activeClassName="list-active">
+                    이용안내
+                  </NavLink>
+                </li>
+                <li className="list-item">
+                  <NavLink to="/" activeClassName="list-active">
+                    FAQ
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                상품 관리
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                템플릿 관리
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/" activeClassName="list-active">
+                설정값
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminSideBar;
