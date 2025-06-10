cd4fe98f4887269b3946ade6a8b43d648b691ce2
최호철 <choiho0130@gmail.com>
2025-06-10 12:46:04 +0900

    commit test

--- react-project/src/components/user/MyPage.jsx
+++ react-project/src/components/user/MyPage.jsx
@@ -2,14 +2,13 @@ import '../../css/user/MyPage.css';
 import SideBar from './myPageComponent/SideBar';
 
 const MyPage = () => {
-   
     return (
-    <div className='MyPage'>
-        <SideBar/>
-            <h1>
-                마이 페이지 입니다.
-            </h1>
-    </div>
+        <div className="MyPage">
+            <div className="container">
+                <SideBar />
+                <h1>마이 페이지 입니다.</h1>
+            </div>
+        </div>
     );
 };
 
