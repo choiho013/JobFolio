29f996cecd06b6e946aa075fc692701fd12eb3d4
kimminhyuck <rhkddka02@naver.com>
2025-06-12 17:16:21 +0900

    Refactor UserController, UserService, and UserVO to standardize parameter naming conventions and enhance error handling. Updated UserMapper and various XML mappers to reflect changes in property names. Improved JoinForm component by adding detailed address input and integrating Daum address search functionality. Enhanced validation and error messaging for better user experience in both JoinForm and LoginForm components.

--- jobfollio/src/main/java/com/happyjob/jobfolio/vo/join/UserVO.java
+++ jobfollio/src/main/java/com/happyjob/jobfolio/vo/join/UserVO.java
@@ -3,100 +3,100 @@ package com.happyjob.jobfolio.vo.join;
 import java.util.Date;
 
 public class UserVO {
-    private String loginId;        // 사용자ID(이메일)
-    private String userType;       // 사용자구분 (예: 'C' - 일반사용자, 'B' - 하위 관리자 'A' - 최상 관리자)
-    private String userName;       // 이름
+    private String login_id;        // 사용자ID(이메일)
+    private String user_type;       // 사용자구분 (예: 'C' - 일반사용자, 'B' - 하위 관리자 'A' - 최상 관리자)
+    private String user_name;       // 이름
     private String password;       // 비밀번호
     private String birthday;       // 생년월일
     private String sex;           // 성별
     private String hp;            // 연락처
-    private String regDate;       // 가입일자
-    private String withdrawalDate; // 탈퇴일자
-    private String statusYn;      // 활퇴여부 ('Y' - 탈퇴, 'N' - 탈퇴아님)
-    private Date expireDays;      // 구독만료일까지
+    private String reg_date;       // 가입일자
+    private String withdrawal_date; // 탈퇴일자
+    private String status_yn;      // 활퇴여부 ('Y' - 탈퇴, 'N' - 탈퇴아님)
+    private Date expire_days;      // 구독만료일까지
     private String address;       // 주소
     private String hobby;         // 취미/특기
     private String notes;         // 특이사항
-    private Long userNo;          // 회원일련번호 (PK)
+    private Long user_no;          // 회원일련번호 (PK)
 
     // 기본 생성자
     public UserVO() {}
 
     // 간소화된 생성자
-    public UserVO(String loginId, String userType, String userName, String password,
-                  String birthday, String sex, String hp, String regDate,
-                  String withdrawalDate, String statusYn, Date expireDays,
-                  String address, String hobby, String notes, Long userNo) {
-        this.loginId = loginId;
-        this.userType = userType;
-        this.userName = userName;
+    public UserVO(String login_id, String user_type, String user_name, String password,
+                  String birthday, String sex, String hp, String reg_date,
+                  String withdrawal_date, String status_yn, Date expire_days,
+                  String address, String hobby, String notes, Long user_no) {
+        this.login_id = login_id;
+        this.user_type = user_type;
+        this.user_name = user_name;
         this.password = password;
         this.birthday = birthday;
         this.sex = sex;
         this.hp = hp;
-        this.regDate = regDate;
-        this.withdrawalDate = withdrawalDate;
-        this.statusYn = statusYn;
-        this.expireDays = expireDays;
+        this.reg_date = reg_date;
+        this.withdrawal_date = withdrawal_date;
+        this.status_yn = status_yn;
+        this.expire_days = expire_days;
         this.address = address;
         this.hobby = hobby;
         this.notes = notes;
-        this.userNo = userNo;
+        this.user_no = user_no;
     }
 
     // Getter 메서드들
-    public String getLoginId() { return loginId; }
-    public String getUserType() { return userType; }
-    public String getUserName() { return userName; }
+    public String getLogin_id() { return login_id; }
+    public String getUser_type() { return user_type; }
+    public String getUser_name() { return user_name; }
     public String getPassword() { return password; }
     public String getBirthday() { return birthday; }
     public String getSex() { return sex; }
     public String getHp() { return hp; }
-    public String getRegDate() { return regDate; }
-    public String getWithdrawalDate() { return withdrawalDate; }
-    public String getStatusYn() { return statusYn; }
-    public Date getExpireDays() { return expireDays; }
+    public String getReg_date() { return reg_date; }
+    public String getWithdrawal_date() { return withdrawal_date; }
+    public String getStatus_yn() { return status_yn; }
+    public Date getExpire_days() { return expire_days; }
     public String getAddress() { return address; }
     public String getHobby() { return hobby; }
     public String getNotes() { return notes; }
-    public Long getUserNo() { return userNo; }
+    public Long getUser_no() { return user_no; }
 
     // Setter 메서드들
-    public void setLoginId(String loginId) { this.loginId = loginId; }
-    public void setUserType(String userType) { this.userType = userType; }
-    public void setUserName(String userName) { this.userName = userName; }
+    public void setLogin_id(String login_id) { this.login_id = login_id; }
+    public void setUser_type(String user_type) { this.user_type = user_type; }
+    public void setUser_name(String user_name) { this.user_name = user_name; }
     public void setPassword(String password) { this.password = password; }
     public void setBirthday(String birthday) { this.birthday = birthday; }
     public void setSex(String sex) { this.sex = sex; }
     public void setHp(String hp) { this.hp = hp; }
-    public void setRegDate(String regDate) { this.regDate = regDate; }
-    public void setWithdrawalDate(String withdrawalDate) { this.withdrawalDate = withdrawalDate; }
-    public void setStatusYn(String statusYn) { this.statusYn = statusYn; }
-    public void setExpireDays(Date expireDays) { this.expireDays = expireDays; }
+    public void setReg_date(String reg_date) { this.reg_date = reg_date; }
+    public void setWithdrawal_date(String withdrawal_date) { this.withdrawal_date = withdrawal_date; }
+    public void setStatus_yn(String status_yn) { this.status_yn = status_yn; }
+    public void setExpire_days(Date expire_days) { this.expire_days = expire_days; }
     public void setAddress(String address) { this.address = address; }
     public void setHobby(String hobby) { this.hobby = hobby; }
     public void setNotes(String notes) { this.notes = notes; }
-    public void setUserNo(Long userNo) { this.userNo = userNo; }
+    public void setUser_no(Long user_no) { this.user_no = user_no; }
 
     // toString 메서드 (깔끔하게 정리)
     @Override
     public String toString() {
         return "UserVO{" +
-                "loginId='" + loginId + '\'' +
-                ", userType='" + userType + '\'' +
-                ", userName='" + userName + '\'' +
+                "login_id='" + login_id + '\'' +
+                ", user_type='" + user_type + '\'' +
+                ", user_name='" + user_name + '\'' +
                 ", password='" + password + '\'' +
                 ", birthday='" + birthday + '\'' +
                 ", sex='" + sex + '\'' +
                 ", hp='" + hp + '\'' +
-                ", regDate='" + regDate + '\'' +
-                ", withdrawalDate='" + withdrawalDate + '\'' +
-                ", statusYn='" + statusYn + '\'' +
-                ", expireDays=" + expireDays +
+                ", reg_date='" + reg_date + '\'' +
+                ", withdrawal_date='" + withdrawal_date + '\'' +
+                ", status_yn='" + status_yn + '\'' +
+                ", expire_days=" + expire_days +
                 ", address='" + address + '\'' +
                 ", hobby='" + hobby + '\'' +
                 ", notes='" + notes + '\'' +
-                ", userNo=" + userNo +
+                ", user_no=" + user_no +
                 '}';
     }
 }
