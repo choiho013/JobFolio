8d25875a39a59b7bceaf1e8319bcc99c0486eb4e
Rocomi <kauli1014@naver.com>
2025-06-17 17:51:15 +0900

    Update SecurityConfig.java
    쉼표 추가합니다

--- jobfollio/src/main/java/com/happyjob/jobfolio/config/SecurityConfig.java
+++ jobfollio/src/main/java/com/happyjob/jobfolio/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                         "/api/product/**",
                         "/api/resume/**",             // 결제 관련
                         "/api/board/user/info/list",   // 이용안내 페이지
-                        "/api/board/user/faq/list"    // faq 페이지
+                        "/api/board/user/faq/list",    // faq 페이지
                         "/resumes/**",
                         "/api/community/**",
                         "/error"                  // 에러 페이지
